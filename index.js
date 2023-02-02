83ebcd98efae6012e67dd3a8fc7645c1dd5ed2ec
Emily <easchoettler@gmail.com>
2023-02-02 16:15:48 -0500

    Add reverse string function

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ export function capitalize(str) {
     arr.splice(0,1,firstLetter);
     return arr.join('');
 }
+
+export function reverseString(str) {
+    let arr = str.split('');
+    arr.reverse();
+    return arr.join('');
+}
