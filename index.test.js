9280e5122363781b1208df7dc47ccac1c5dbd9f0
Emily <easchoettler@gmail.com>
2023-02-02 16:25:18 -0500

    Add calculator method tests

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./index";
+import { capitalize, reverseString, calculator } from "./index";
 
 test('capitalizes first character of string', () => {
     expect(capitalize('please capitalize me')).toMatch(/Please capitalize me/);
@@ -6,4 +6,20 @@ test('capitalizes first character of string', () => {
 
 test('reverses string', () => {
     expect(reverseString('reverse me')).toMatch(/em esrever/);
+});
+
+test('calculator addition', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('calculator subtraction', () => {
+    expect(calculator.subtract(5, 3)).toBe(2);
+});
+
+test('calculator divide', () => {
+    expect(calculator.divide(15, 3)).toBe(5);
+});
+
+test('calculator multiply', () => {
+    expect(calculator.multiply(10, 3)).toBe(30);
 });
