5b7de72b6a52c3132e3ef82fa5bca16a37552d60
Emily <easchoettler@gmail.com>
2023-02-02 16:12:23 -0500

    Add import statement for capitalize function

--- index.test.js
+++ index.test.js
@@ -1,3 +1,5 @@
+import { capitalize } from "./index";
+
 test('capitalizes first character of string', () => {
     expect(capitalize('please capitalize me')).toMatch(/Please capitalize me/);
-});
+}); 
