02ddd0087e85ccdb45a5aa95e3e6c755284f082e
Emily <easchoettler@gmail.com>
2023-02-02 16:14:23 -0500

    Add reverse string function test

--- index.test.js
+++ index.test.js
@@ -3,3 +3,7 @@ import { capitalize } from "./index";
 test('capitalizes first character of string', () => {
     expect(capitalize('please capitalize me')).toMatch(/Please capitalize me/);
 }); 
+
+test('reverses string', () => {
+    expect(reverseString('reverse me')).toMatch(/em esrever/);
+});
